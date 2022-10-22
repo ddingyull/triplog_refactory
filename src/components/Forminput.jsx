402f84c89be7eead2fb2e79efd63621888f629cf
yurim <dding.yull@gmail.com>
2022-10-22 20:03:09 +0900

    style(components):공통컴포넌트 생성 및 수정

--- src/components/Forminput.jsx
+++ src/components/Forminput.jsx
@@ -1,24 +1,15 @@
-import React from 'react';
+import { Form } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import styled from 'styled-components';
 
-  return (
-    <div className="mb-4">
-      <label
-        className="block text-gray-700 text-sm font-bold mb-2"
-      >
-        {label}
-      </label>
-      <input
-        id={id}
-        className="shadow border rounded w-full py-2 px-3 text-gray-700"
-        ref={inputRef}
-        value={formData[id]}
-        onChange={(e) => setFormData({ ...formData, [id]: e.target.value })}
-        onBlur={() => console.log(checkRegex())}
-        {...inputProps}
-      />
-      <div className="mt-1 mb-3 text-xs text-red-500">에러메시지</div>
-    </div>
-  );
-
-
-export default FormInput;
+export default function Forminput({id, errMessage}) {
+  return(
+    <Form className='mb-2'>
+      <Form.Label className='d-block mb-1'>{id}</Form.Label>
+      <Form.Control type="email" placeholder="이메일 입력" className='shadow-sm p-3 w-full border' />
+      <Form.Text className="text-danger m-1">
+          {errMessage}
+      </Form.Text>
+    </Form>
+  )
+}
