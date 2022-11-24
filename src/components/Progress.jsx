f30cf51f98e59d3fb2b8839de3724a3cccf2d03c
lzns960 <lzns960@gmail.com>
2022-11-24 18:14:36 +0900

    fix(detail): 평균별점 수정

--- src/components/Progress.jsx
+++ src/components/Progress.jsx
@@ -2,21 +2,54 @@ import { FaStar } from 'react-icons/fa';
 import styled from 'styled-components';
 
 export default function Progress({ starAvg }) {
-  const rating = (starAvg) => {
-    const result = [];
-    for (let i = 5; i > 0; i--) {
-      starAvg--;
-      if (starAvg >= 0) {
-        result.push(<FaStar size="20" color="#fcc419"></FaStar>);
-      } else {
-        result.push(<FaStar size="20" color="lightgray"></FaStar>);
-      }
-    }
-    return result;
-  };
+  // const rating = (starAvg) => {
+  //   const result = [];
+  //   for (let i = 5; i > 0; i--) {
+  //     starAvg--;
+  //     if (starAvg >= 0) {
+  //       result.push(<FaStar size="20" color="#fcc419"></FaStar>);
+  //     } else {
+  //       result.push(<FaStar size="20" color="lightgray"></FaStar>);
+  //     }
+  //   }
+  //   return result;
+  // };
   return (
     <>
-      {rating(starAvg)} {starAvg} / 5
+      <StarWrap>
+        <span class="star">
+          <FaStar size="20"></FaStar>
+          <FaStar size="20"></FaStar>
+          <FaStar size="20"></FaStar>
+          <FaStar size="20"></FaStar>
+          <FaStar size="20"></FaStar>
+          <span class="starAvg" style={{ width: `${starAvg * 20}%` }}>
+            <FaStar size="20"></FaStar>
+            <FaStar size="20"></FaStar>
+            <FaStar size="20"></FaStar>
+            <FaStar size="20"></FaStar>
+            <FaStar size="20"></FaStar>
+          </span>
+        </span>{' '}
+      </StarWrap>
+      {starAvg} / 5
     </>
   );
 }
+
+const StarWrap = styled.span`
+  .star {
+    position: relative;
+    color: lightgray;
+
+    .starAvg {
+      white-space: nowrap;
+      position: absolute;
+      overflow: hidden;
+
+      left: 0;
+      color: #fcc419;
+      pointer-events: none;
+    }
+  }
+`;
