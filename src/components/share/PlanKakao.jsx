ff98d81440c93c935a5ef963a95c754fd45847f4
yurim <dding.yull@gmail.com>
2022-12-05 19:50:53 +0900

    주석 삭제 추가 수정

--- src/components/share/PlanKakao.jsx
+++ src/components/share/PlanKakao.jsx
@@ -4,9 +4,6 @@ import { Badge } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function PlanKakao() {
-  //현재 url가져오기
-  // const currentUrl = window.location.href;
-
   useEffect(() => {
     initKakao(); //
   }, []);
@@ -23,7 +20,6 @@ export default function PlanKakao() {
 
   //버튼을 누르면 실행되는 함수
   const shareKakao = () => {
-    //이부분이 매우 헷갈림 여러 사이트를 참고했는데 이 sendDefault부분을 잘 봐야한다.
     window.Kakao.Link.sendDefault({
       objectType: 'feed',
       content: {
